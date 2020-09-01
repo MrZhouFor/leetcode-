0049cf0c302be6d51fa7045716ddae9a6f8357ae
zack <ze.zhou@whats-egg.com>
2020-09-01 09:58:04 +0800

    c

---
+++ src/main/java/app/all/easy206/ListNode.java
@@ -0,0 +1,14 @@
+package app.all.easy206;
+
+/**
+ * @author zhouze
+ * @date 2020/8/11
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/app/all/easy206/Solution.java
@@ -0,0 +1,35 @@
+package app.all.easy206;
+
+/**
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhouze
+ * @date 2020/8/11
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null){
+            return head;
+        }
+        ListNode cur = head;
+        ListNode prev = null;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
