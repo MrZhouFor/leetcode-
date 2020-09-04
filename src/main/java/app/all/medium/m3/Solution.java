ca53ee4e6a534b4fd233a41d2697a1e8684b6226
zack <ze.zhou@whats-egg.com>
2020-09-04 20:58:41 +0800

    commit all

--- src/main/java/app/all/medium/m3/Solution.java
+++ src/main/java/app/all/medium/m3/Solution.java
@@ -1,5 +1,8 @@
 package app.all.medium.m3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 3. 无重复字符的最长子串
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -25,9 +28,18 @@ package app.all.medium.m3;
  */
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        for (int i = 0; i < s.length(); i++) {
-
+        int len = s.length();
+        Map<Character, Integer> maps = new HashMap<>();
+        int answer = 0, left = 0;
+        for (int i = 0; i < len; i++) {
+            char curr = s.charAt(i);
+            if (maps.containsKey(curr)) {
+                left = Math.max(left, maps.get(curr) + 1);
+            }
+            maps.put(curr, i);
+            answer = Math.max(answer, i - left + 1);
         }
-        return 0;
+        return answer;
     }
+
 }
