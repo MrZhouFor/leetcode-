758eee09fad1dc2a5440e88709fc16c51b8e9111
zack <ze.zhou@whats-egg.com>
2021-01-30 22:47:22 +0800

    add .

--- src/leetcode/editor/cn/[11]盛最多水的容器.java
+++ src/leetcode/editor/cn/[11]盛最多水的容器.java
@@ -77,6 +77,7 @@ class Solution {
     public int maxArea(int[] height) {
         int max = 0;
         for (int i = 0, j = height.length - 1; i < j; ) {
+            // 计算左右两边的值，哪边小，哪边先移动
             int minHeight = height[i] < height[j] ? height[i++] : height[j--];
             max = Math.max(max, (j - i + 1) * minHeight);
         }
