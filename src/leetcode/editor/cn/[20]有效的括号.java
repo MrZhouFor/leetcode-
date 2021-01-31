c9ef76e95b819bdbe13e827bf5b1ecf9acdd5500
zack <ze.zhou@whats-egg.com>
2021-01-31 15:05:41 +0800

    add .

---
+++ src/leetcode/editor/cn/[20]有效的括号.java
@@ -0,0 +1,89 @@
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。 
+//
+// 有效字符串需满足： 
+//
+// 
+// 左括号必须用相同类型的右括号闭合。 
+// 左括号必须以正确的顺序闭合。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "()"
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "()[]{}"
+//输出：true
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "(]"
+//输出：false
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：s = "([)]"
+//输出：false
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：s = "{[]}"
+//输出：true 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 104 
+// s 仅由括号 '()[]{}' 组成 
+// 
+// Related Topics 栈 字符串 
+// 👍 2128 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// 1.暴力：不断replace匹配的括号. O(n^2)
+// 2. 栈
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+        Map<Character, Character> pairs = new HashMap<>() {
+            {
+                put('}', '{');
+                put(']', '[');
+                put(')', '(');
+            }
+        };
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (pairs.containsKey(c)) {
+                if (stack.isEmpty() || stack.peek() != pairs.get(c)) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
